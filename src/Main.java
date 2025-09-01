0cc8bea2f93d5df1760c43985a29559b8b2d67fb
TaeXtream <59652919+taextreme@users.noreply.github.com>
2025-09-01 15:09:06 +0700

    feat: Make Main Class a record with robot and table

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public record Main(Robot robot, Table table) {
+
     public static void main(String[] args) {
+        Table table = new Table(5, 5);
+        Robot robot = new Robot();
+        Main app = new Main(robot, table);
 
     }
+
 }
